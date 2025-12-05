6afad08995c18eb5353477267a3b770d8bc9f0a0
helloaaditya <aadityakum123@gmail.com>
2025-12-05 14:31:01 +0530

    df

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:3000',
   'https://prod.sanjanawaterproofing.com',
-  'https://crm-1ej7.onrender.com',
+  'https://crm-chi-rouge.vercel.app',
   'https://localhost',  // Capacitor Android/iOS
   'capacitor://localhost',  // Capacitor iOS
   'ionic://localhost',  // Ionic
